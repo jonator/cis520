a02bc2d8a3b8b7c314ce6b8e619391a421f9ce4f
Brandon Fisher <bmfisher@cougar.cs.ksu.edu>
2020-02-13 21:58:48 -0600

    Re-implemented timer_sleep using thread list
    Co-authored-by: Jon Ator <jonathanator0@gmail.com>
    Co-authored-by: milesz8 <miles.mclenon@gmail.com>

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,6 +88,8 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int64_t wakeup_tick;                /* Wakeup_tick for sleeping*/
+    struct list_elem sleep_elem;        /* List element for sleep_list in timer.c*/
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
